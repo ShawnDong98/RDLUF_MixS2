8d50833136466a97a515074e134c3742df63e6f2
ShawnDong98 <ShawnDong98@gmail.com>
2022-11-15 09:53:55 +0800

    realse code for RDLUF_MixS2

--- real/train_code/train.py
+++ real/train_code/train.py
@@ -23,14 +23,6 @@ import losses
 from schedulers import get_cosine_schedule_with_warmup
 from pprint import pprint
 
-import wandb
-
-wandb.init(
-    project = "DLUF_MixS2_Real",
-    config = opt,
-    name = opt.exp_name,
-    entity = "iisc-xdu"
-)
 
 
 
@@ -162,7 +154,6 @@ def test():
         result = result.unsqueeze(1)
         result = torch.flip(result, [2, 3])
         result = make_grid(result, nrows=4) # torch 1.12
-        result = wandb.Image(result, caption=f"scene{j}")
 
         image_log[f'scene{j}'] = [result]
 
@@ -172,10 +163,6 @@ def test():
 if __name__ == "__main__":
     ## pipline of training
     for epoch in range(start_epoch+1, opt.max_epoch):
-        log_dict = {}
         train_loss = train()
-        log_dict['train_loss'] = train_loss
         image_log = test()
-        log_dict.update(image_log)
-        wandb.log(log_dict)
         torch.cuda.empty_cache()
